8106ed20e638ae10c739c77e1dd6fc1098db33e7
Maciej Marciniak <maciej.r.marciniak@student.put.poznan.pl>
2017-05-03 19:40:14 +0200

    przebudowanie kodu na wielewątkowy

--- Wykrywanie i zliczanie twarzy.py
+++ Wykrywanie i zliczanie twarzy.py
@@ -1,44 +1,73 @@
 #!/usr/bin/python
 import time
 import cv2
+import imutils
 from picamera.array import PiRGBArray
 from picamera import PiCamera
+import thread
 
 # inicjalizacja parametrow kamery
-camera = PiCamera()
-camera.resolution = (320, 240)
-camera.framerate = 30
-rawCapture = PiRGBArray(camera, size=(320, 240))
+camera = PiCamera(
+    resolution=(640, 480))
+camera.iso = 800
+camera.framerate = 90
+camera.led = False
+rawCapture = PiRGBArray(camera, size=(640, 480))
 camera.rotation = 180
-camera.video_stabilization = True
+camera.brightness = 60
 time.sleep(0.25)
+
+camera.start_preview()
+time.sleep(2)
+
+# pobierz bierzaca klatke
+camera.capture(rawCapture, format='bgr', use_video_port=True)
+frame = rawCapture.array
+rawCapture.truncate(0)
+
+faces = []
+
+def make_frame():
+    global frame
+    while True:
+        # pobierz bierzaca klatke
+        camera.capture(rawCapture, format='bgr', use_video_port=True)
+        frame = rawCapture.array
+        rawCapture.truncate(0)
+
+def detect_faces():
+    global frame
+    global faces
+    while True:
+        # przekonwertuj na odcienie szarosci
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # wykryj twarze
+        faces = face_cascade.detectMultiScale(gray, 1.2, 6)
+        # wyroznij twarze umieszczajac je w ramkach
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        # dodaj do klatki tekst mowiacy o ilosci wykrytych twarzy
+        cv2.putText(frame, "Wykryto: {} twarzy".format(len(faces)), (10, 20),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+
 
 # Zaladuj plik z klasyfikatorami do wykrycia twarzy
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
 
+thread.start_new_thread(make_frame,())
+thread.start_new_thread(detect_faces,())
 while True:
-    # pobierz bierzaca klatke
-    camera.capture(rawCapture, format='bgr', use_video_port=True)
-    frame = rawCapture.array
-    rawCapture.truncate(0)
-
-    # wykryj twarze
-    faces = face_cascade.detectMultiScale(frame, 1.2, 6)
-
-    # dodaj do klatki tekst mowiacy o ilosci wykrytych twarzy
-    cv2.putText(frame, "Wykryto: {} twarzy".format(str(len(faces))), (10, 20),
-        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-
     # wyroznij twarze umieszczajac je w ramkach
-    for (x,y,w,h) in faces:
-        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
-
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    # dodaj do klatki tekst mowiacy o ilosci wykrytych twarzy
+    cv2.putText(frame, "Wykryto: {} twarzy".format(len(faces)), (10, 20),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
     cv2.imshow("Wykrywanie twarzy", frame)
-
     # zamykanie programu po wcisnieciu ESC
     key = cv2.waitKey(10)
     if key == 27:
         cv2.destroyWindow("Wykrywanie twarzy")
         break
-
+camera.stop_preview()
 cv2.destroyAllWindows()
