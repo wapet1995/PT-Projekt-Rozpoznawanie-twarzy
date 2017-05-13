4385c1178091962c8f4f10af77032ee9db2a1f83
damifil <damifil15@gmail.com>
2017-05-13 20:01:34 +0200

    czesciowa zmiana face recognizer
    dodana funkcja (w trakcie robienia) do dodawania zdjec do teningu
    poprawione wykrywanie nazwy pliku z imagepath

--- face_recognizer.py
+++ face_recognizer.py
@@ -15,6 +15,42 @@ recognizer = cv2.createFisherFaceRecognizer()
 witdh_face = 250
 hight_face = 250
 
+def add_image_to_train_set(choise):
+    x=200
+    y=200
+    w=640
+    h=640
+    name = raw_input("Podaj imie: ")
+    vorname=raw_input("Podaj nazwisko: ")
+    index=100
+    quantity=0
+
+    #polaczenie z BD
+    #conn = MySQLdb.connect(host="localhost", user="root", passwd="inteligentnyzamek", db="Rozpoznawanie_twarzy_db")
+    #c = conn.cursor()
+    #if choise == "1":
+       #wyszukanie id osoby
+    #else:
+        #zapisujemy w bd
+        # odczytujemy nowy numer i wpisujemy do zmiennej index
+
+    count =0
+    print "ustaw sie w bialej ramce jak juz bedziesz nacisnij przycisk"
+    while(count<10):
+        #cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 0), 2)
+        print "jesli jestes tylko ty w ramce nacisnij jakis przycisk"
+        cv2.waitkey()
+
+        #sprawdzic ile jest twarzy i odczytac
+
+        #jesli jest ilosc twarzy rowna 1
+        if(quantity==1):
+            count=count+1
+
+            # zapisanie do zmiennej image image
+            #zapisanie zdjecia w folderze
+
+    #dodalismy zdjecia mozna przejsc do innej funkcji z treningiem
 
 def get_images_and_labels(path):
     # format pliku (nazwa twarzy [subjectNUMER] po kropce wyraz twarzy
@@ -30,48 +66,30 @@ def get_images_and_labels(path):
         # konwersja na numpy array
         image = np.array(image_pil, 'uint8')
         # uzyskanie numeru twarzy z nazwy pliku
-        nbr = int(image_path[20:22])
-        # wykrycie twarzy na zdjeciu (skopiowane od Macieja
+        base = os.path.basename(image_path)
+        nbr = int(base[6:8])
+        print nbr
+        print "przewa"
         faces = faceCascade.detectMultiScale(image)
-        # faces = faceCascade.detectMultiScale(image)
-        # na wykrytej twarzy
         for (x, y, w, h) in faces:
             images.append(image[y: y + hight_face, x: x + witdh_face])
-            print(image_path)
+            #print(image_path)
             # dodanie etykiety
             labels.append(nbr)
-            # wyswietlenie dodanej twarzy
-            cv2.imshow("Dodawanie zdjęć", image[y: y + hight_face, x: x + witdh_face])
     return images, labels
 
 
 if __name__ == '__main__':
     # sciezka dostepu do plikow z zdjeciami (do nauki)
+    #choise = raw_input("jesli chcesz dodac zdjecie do danej osoby wpisz 1 jesli nowa osobe chcesz dodac wpisz 2: ")
+    #add_image_to_train_set(choise)
+
     path = './testowe_zdjecia'
-    # Call the get_images_and_labels function and get the face images and the
-    # corresponding labels
-    '''images, labels = get_images_and_labels(path)
-    cv2.destroyAllWindows()
+    images, labels = get_images_and_labels(path)
+    get_images_and_labels(path)
 
-    # wykonanie treningu
+    # wykonanie treningu i zapisanie
     recognizer.train(images, np.array(labels))
-    recognizer.save("wytrenowany_plik.mdl")'''
-    recognizer.load("wytrenowany_plik.mdl")
+    recognizer.save("wytrenowany_plik.mdl")
+    print "koniec treningu"
 
-    image_paths = [os.path.join(path, f) for f in os.listdir(path) if f.endswith('.test')]
-    for image_path in image_paths:
-        predict_image_pil = Image.open(image_path).convert('L')
-        print(image_path)
-        predict_image = np.array(predict_image_pil, 'uint8')
-
-        faces = faceCascade.detectMultiScale(predict_image)
-
-        for (x, y, w, h) in faces:
-            cropped = predict_image[y: y + hight_face, x: x + witdh_face].copy()
-            nbr_predicted, conf = recognizer.predict(cropped)
-            nbr_actual = int(image_path[20:22])
-            if nbr_actual == nbr_predicted:
-                print "{} poprawnie rozpoznane z dokladnoscia ) {}".format(nbr_actual, conf)
-            else:
-                print "{} niepoprawne rozpoznanie {}".format(nbr_actual, nbr_predicted)
-            cv2.imshow("twarz ktora probowano rozpoznac", predict_image[y: y + hight_face, x: x + witdh_face])
